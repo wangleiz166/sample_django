afc98f6725befb7a36e48e56c651fa5820afc07d
wangleiz166 <wangleiz166@126.com>
2023-03-09 09:43:37 +0000

    init file

---
+++ temp_stories/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from faker import Faker
+
+# Create your views here.
+
+
+# Create your views here.
+# use f-strings for easy string formatting https://realpython.com/python-f-strings/ 
+
+def story():
+            fake = Faker()
+            mystory = (
+                f"In a(n) {fake.company()} a young {fake.language_name()} " 
+                f"stumbles across a(n) {fake.domain_word()} which spurs him into conflict with {fake.name() }"
+                f"an {fake.catch_phrase()} with the help of a(n) {fake.job()} and her {fake.file_name()}"
+                f" culminating in a struggle at {fake.company()} where someone shouts: '{fake.bs()}'"
+                )
+            return mystory
+
+# def index(request):
+#             mystory = story()
+#             return render(request, 'temp_stories/index.html', {'story': mystory})   
+
+def index(request):
+            mystory = story()
+            converted_t = None
+            temp = None
+            if request.method== "POST":
+                    temp = int(request.POST.get('temp',''))
+                    converted_t = (temp-32)*0.5556
+            return render(request, 'temp_stories/index.html', {'story': mystory, 'converted_t': converted_t, 'temp': temp})
